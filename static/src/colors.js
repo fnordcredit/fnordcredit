6016ed92cf79d3ff76828211887b9dbf977e7ac5
uwap <me@uwap.name>
2017-09-28 19:21:37 +0200

    Pinpad: store pin temporary

--- static/src/colors.js
+++ static/src/colors.js
@@ -2,6 +2,7 @@
 import * as MuiColors from "material-ui/styles/colors";
 
 export const background = "#4e5d6c";
+export const lightBackground = "#637f7a";
 export const textColor = MuiColors.blueGrey50;
 export const active = MuiColors.teal200;
 export const red = MuiColors.red600;
--- static/src/components/pinpad.js
+++ static/src/components/pinpad.js
@@ -1,26 +1,57 @@
 import React from "react";
 import Modal from "./modal";
-import RaisedButton from "material-ui/RaisedButton";
+import FlatButton from "material-ui/FlatButton";
+import TextField from "material-ui/TextField";
 import * as Colors from "../colors";
 
-export default class PinPad extends React.Component<{},void> {
+type State = {
+  pin: string
+};
+
+export default class PinPad extends React.Component<{},State> {
   constructor(props: {}) {
     super(props);
+
+    this.state = {
+      pin: ""
+    };
+  }
+
+  appendPin = (c: string) => {
+    this.setState({pin: this.state.pin + c});
   }
 
   render = () => {
     return (
       <Modal>
         <div>
+          <TextField
+            floatingLabelText="Pin"
+            type="password"
+            disabled={true}
+            value={this.state.pin}
+          />
           {[[1,2,3],[4,5,6],[7,8,9]].map(
             ns => (<div key={`${ns[0]}-${ns[2]}`}>{ns.map(
-              n => <RaisedButton label={n} key={n} backgroundColor={Colors.background} labelColor={Colors.textColor} />
+              n => <FlatButton label={n} key={n} backgroundColor={Colors.background} onClick={() => this.appendPin(n)} {...buttonStyle} />
             )}</div>))}
-          <RaisedButton label="X" backgroundColor={Colors.red} labelColor={Colors.textColor} />
-          <RaisedButton label="0" backgroundColor={Colors.background} labelColor={Colors.textColor} />
-          <RaisedButton label="OK" backgroundColor={Colors.green} labelColor={Colors.textColor} />
+          <FlatButton label="X" backgroundColor={Colors.red} {...buttonStyle} />
+          <FlatButton label="0" backgroundColor={Colors.background} onClick={() => this.appendPin(0)} {...buttonStyle} />
+          <FlatButton label="OK" backgroundColor={Colors.green} {...buttonStyle} />
         </div>
       </Modal>
     );
   }
+};
+
+const buttonStyle = {
+  style: {
+    width: 120,
+    height: 90
+  },
+  labelStyle: {
+    fontSize: 26,
+    color: Colors.textColor
+  },
+  hoverColor: Colors.lightBackground
 };
