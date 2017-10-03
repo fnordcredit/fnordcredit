eefa53c6ef07d5973ec14690ae126b32da1d8f75
uwap <me@uwap.name>
2017-10-03 06:03:24 +0200

    Update webpack configuration

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,7 +1,11 @@
 // webpack.config.js:
 
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+const WebpackShellPlugin = require('webpack-shell-plugin');
+
+const usingNixOS = process.env.NIX_PROFILES != undefined;
+const preBuildScripts = usingNixOS ? [] : ['flow'];
 
 module.exports = {
   resolve: {
@@ -11,15 +15,19 @@ module.exports = {
     path.resolve(__dirname, 'src/index.js')
   ],
   output: {
-    path: path.resolve(__dirname, 'public/dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
     publicPath: 'dist/'
   },
   module: {
     loaders: [
-      { test: /\.css$/, loader: "style!css" },
+      { test: /\.(woff2?|eot|ttf|svg)$/, loader: "file-loader" },
+      { test: /\.css$/, loader: "style-loader!css-loader" },
       { test: /\.js(x)?$/, exclude: /node_modules/, loader: "babel-loader" }
     ]
   },
+  plugins: [
+    new WebpackShellPlugin({onBuildStart:preBuildScripts})
+  ],
   devtool: 'source-map'
-}
+};
