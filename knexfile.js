faef224484d3f80952556b48f2a8a579d5ef90ea
uwap <me@uwap.name>
2019-01-15 20:21:09 +0100

    Disable mysql support again

--- knexfile.js
+++ knexfile.js
@@ -9,19 +9,8 @@ const sqlite3config = {
   },
 };
 
-const mysqlconfig = {
-  client: 'mysql',
-  useNullAsDefault: true,
-  connection: {
-    host: process.env.DB_HOST || '127.0.0.1',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_DATABASE || 'fnordcredit'
-  }
-};
-
 module.exports = {
   development: sqlite3config,
-  production: process.env.USE_MYSQL ? mysqlconfig : sqlite3config,
+  production: sqlite3config,
   test: sqlite3config,
 };
