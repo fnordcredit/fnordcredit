b0abf60bb3e1a7bda2b28416106e3b0552d6eb3d
uwap <me@uwap.name>
2019-01-29 23:57:53 +0100

    Fix bookshelf models

--- src/Model/ProductModel.js
+++ src/Model/ProductModel.js
@@ -5,13 +5,18 @@ export default class ProductModel extends BaseModel<Product> {
   static tableName = 'products';
   static idAttribute = 'id';
   format(attributes: Object) {
-    attributes.image_path = attributes.imagePath;
+    if (attributes.imagePath) {
+      attributes.image_path = attributes.imagePath;
+      delete attributes.imagePath;
+    }
     attributes.price = Math.floor(attributes.price * 100);
-    delete attributes.imagePath;
     return attributes;
   }
   parse(attributes: Object) {
-    attributes.imagePath = attributes.image_path;
+    if (attributes.image_path) {
+      attributes.imagePath = attributes.image_path;
+      delete attributes.image_path;
+    }
     attributes.price = attributes.price / 100;
     return attributes;
   }
--- src/Model/UserModel.js
+++ src/Model/UserModel.js
@@ -5,17 +5,27 @@ export default class UserModel extends BaseModel<User> {
   static tableName = 'user';
   static idAttribute = 'id';
   format(attributes: Object) {
-    // eslint-disable-next-line
-    attributes.debt_allowed = attributes.debtAllowed;
-    // eslint-disable-next-line
-    attributes.debt_hard_limit = attributes.debtHardLimit;
-    delete attributes.debtAllowed;
-    delete attributes.debtHardLimit;
+    if (attributes.debtAllowed) {
+      // eslint-disable-next-line
+      attributes.debt_allowed = attributes.debtAllowed;
+      delete attributes.debtAllowed;
+    }
+    if (attributes.debtHardLimit) {
+      // eslint-disable-next-line
+      attributes.debt_hard_limit = attributes.debtHardLimit;
+      delete attributes.debtHardLimit;
+    }
     return attributes;
   }
   parse(attributes: Object) {
-    attributes.debtAllowed = attributes.debt_allowed;
-    attributes.debtHardLimit = attributes.debt_hard_limit;
+    if (attributes.debt_allowed) {
+      attributes.debtAllowed = attributes.debt_allowed;
+      delete attributes.debt_allowed;
+    }
+    if (attributes.debt_hard_limit) {
+      attributes.debtHardLimit = attributes.debt_hard_limit;
+      delete attributes.debt_hard_limit;
+    }
     return attributes;
   }
 }
