b5a6b98f482d18d5c7da76ff9712aa9de7160be0
uwap <me@uwap.name>
2019-02-28 10:20:39 +0100

    Use the frontend npm package

--- src/serverInit.js
+++ src/serverInit.js
@@ -13,7 +13,7 @@ function errorHandler(ctx: Koa$Context, next: Function) {
 const koa = new Koa();
 
 koa.use(errorHandler);
-koa.use(Static(`${__dirname}/../fnordcredit-frontend/dist`));
+koa.use(Static(`${__dirname}/../node_modules/fnordcredit-frontend`));
 koa.use(bodyParser());
 
 global.koa = koa;
