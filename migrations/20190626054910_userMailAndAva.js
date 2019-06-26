dc7c6fb2803d067b61124f37b25aa709ea761949
uwap <me@uwap.name>
2019-06-26 06:07:11 +0200

    Add avatar and email migration

---
+++ migrations/20190626054910_userMailAndAva.js
@@ -0,0 +1,24 @@
+exports.up = async function(knex, Promise) {
+  await knex.raw('alter table user rename to user_tmp;');
+  await knex.raw(
+    `
+    CREATE TABLE IF NOT EXISTS "user"
+    (
+      id integer primary key AUTOINCREMENT,
+      credit INT NOT NULL,
+      debt_allowed BOOL NOT NULL,
+      debt_hard_limit INT,
+      lastchanged TIMESTAMP NOT NULL,
+      name TEXT NOT NULL UNIQUE ,
+      pincode TEXT,
+      token TEXT,
+      avatar TEXT,
+      email TEXT
+    );
+    `
+  );
+  await knex.raw('insert into user select *, null, null from user_tmp;');
+  await knex.raw('drop table user_tmp;');
+};
+
+exports.down = function(knex, Promise) {};
